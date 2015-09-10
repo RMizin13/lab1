7501e71e9be3710aa9884f72e68b0ba94ca00bf4
mizin <r.mizin96@gmail.com>
2015-09-10 21:20:23 +0300

    Добавил пробел

---
+++ покупка долларов прога/перевод из метров в километры/main.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+
+{
+	start:
+	setlocale(0, "");
+	double kurs, uah, usd;
+	char letter;
+	
+        cout << "введите актуальный курс доллара" << endl;
+		cin >> kurs;
+		
+
+		cout << "Введите количество Долларов которые вы хотите купить" << endl;
+		cin >> usd;
+			uah = usd * kurs;
+
+			cout << usd << " Доллара(ов) = " << uah << " Гривен, по курсу " << kurs << endl;
+			cout << "\n";
+			cout << " + 2% сбор в пенсионный фонд = " << (uah / 100) * 2 << " гривен." << endl;
+			cout << "Итого: " << (uah)+((uah / 100) * 2) << " гривен." << endl;
+			
+
+			system("pause");
+			return 0;
+}
