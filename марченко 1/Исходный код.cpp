5dd2570b60db563dd7163db91272c322fe4a55d0
mizin <r.mizin96@gmail.com>
2015-09-10 22:25:12 +0300

    + исх код

---
+++ марченко 1/Исходный код.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <string>
+#include <cstdio>
+#include <Windows.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+using namespace std;
+int funcCC();
+int funcPS();
+
+int main(int argc, char** argv)
+{
+	int h;
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	char str[100];
+	start:
+	cout << "Який тип вводу/виводу ви хочете обрати?" << endl;
+	cout << "\n";
+	cout << " Натиснiть 1, якщо cin/cout" << endl;
+	cout << " Натиснiть 2, якщо printf/scanf" << endl;
+	cin >> h;
+	switch (h)
+		case 1:
+	{
+		{
+		cout << "Потоковий ввід і вивід тексту" << endl;
+		cout << "Введіть текстовий рядок : " << endl;
+		gets(str);
+		cout << "Ви ввели рядок:" << str << endl << endl;
+     	}
+		break;
+		
+		case 2:
+		{
+			cout << "Ви ввели рядок:" << str << endl << endl;
+			cout << "-----------------------------------------------------------\n1. Ввід і вивід тексту за допомогою функцій scanf і printf.\n-----------------------------------------------------------\nВведіть текстовий рядок: ";
+			scanf("%s", str);
+			printf("Ви ввели рядок: %s\n\n", str);
+		}
+			break; 
+		default:
+		{
+			if (!cin || h>2 || h<1)
+			{
+				int i;
+				system("cls");
+				cout << "Неправильное значение,попробуйте еще раз." << endl;
+				cout << "Невiрне значення спробуйте ще раз." << endl;
+				cout << "Wrong value, try again." << endl;
+				cout << "" << endl;
+				cin.clear();//восстанавливает поток и делает его работоспособным
+				fflush(stdin);//чистит поток ввода от мусора
+				for (i = 0; i < 500000000; i++);
+				system("cls");
+				goto start;
+
+			}
+		}
+
+}
+	system("pause");
+	return 0;
+}
+
+int funcCC()
+{
+
+}
