7d5d77675b57baf9708a2111225e51b3f58bd722
mizin <r.mizin96@gmail.com>
2015-09-11 00:47:10 +0300

    +1 функция , небольшие правки, вприн готовая прога

--- марченко 1/Исходный код.cpp
+++ марченко 1/Исходный код.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <cstdio>
 #include <Windows.h>
-#include<string>
 
 using namespace std;
 int funcCC();
 int funcPS();
-char str[100];
+int end();
+char mass[100];
 
 int main()
 {
@@ -15,21 +15,32 @@ int main()
 	int h;
 	
 	start:
-	cout << "Який тип вводу/виводу ви хочете обрати?" << endl;
-	cout << "\n";
-	cout << " Натиснiть 1, якщо cin/cout" << endl;
-	cout << " Натиснiть 2, якщо printf/scanf" << endl;
+	cout << "Который тип ввода/вывода вы хотите выбрать" << endl;
+	cout << endl;
+	cout << " Нажмите 1, если cin/cout" << endl;
+	cout << " Нажмите 2, если printf/scanf" << endl;
+	cout << " Нажмите 3, для выхода из программы" << endl;
 	cin >> h;
+
 	if (h == 1)
 	{
 		funcCC();
 	}
+
 	else
 		if (h == 2)
 		{
 			funcPS();
+				
 		}
+
 		else
+			if (h == 3)
+			{
+				end();
+			}
+
+		  else
 			if (!cin || h>2 || h<1)
 			{
 				int i;
@@ -52,32 +63,36 @@ int main()
 
 int funcCC()
 {
-	
-	
+	    system("cls");
 		cout << "Введите строку на любом из 3х языков (укр, рус, eng) : " << endl;
 		fflush(stdin);
-		gets(str);
-		
-		cout << "Ви ввели строку:" << str << endl;
+		gets(mass);
+		cout << "Ви ввели строку:" << mass << endl;
 		cout << "" << endl;
-		system("pause");
-		return 0;
 
-	
+    system("pause");
+    return 0;
 }
 
 int funcPS()
 {
-	
-		
+	    system("cls");
 		printf("\nВвод и вывод текста с помощью scanf і printf\n");
 		printf("\nВведите строку на любом из 3х языков (укр, рус, eng) : \n");
 		fflush(stdin);
-		gets(str);
-		printf("Ви ввели рядок: %s \n\n", str);
+		gets(mass);
+		printf("Ви ввели рядок: %s \n\n", mass);
 		
-	
-
 	system("pause");
+	return 0;
+}
+
+int end()
+{
+	    int i;
+	    system("cls");
+	    cout << endl;
+	    cout << "Всего доброго!" << endl<<endl;
+	
 	return 0;
 }
